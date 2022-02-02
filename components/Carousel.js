2ba0aed9e2737b17f7ced4596da6aa94d122b847
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-02-02 16:32:48 +0600

    slider edit

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,105 +1,46 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-
-const Carousel = () => {
+const HCarousel = () => {
   return (
-    <div>
-      <div
-        id="carouselDarkVariant"
-        className="carousel slide carousel-fade carousel-dark relative"
-        data-bs-ride="carousel"
+    <div className="relative mt-5">
+      <div className="absolute w-full bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20"></div>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
       >
-        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-          <button
-            data-bs-target="#carouselDarkVariant"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            data-bs-target="#carouselDarkVariant"
-            data-bs-slide-to="1"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            data-bs-target="#carouselDarkVariant"
-            data-bs-slide-to="2"
-            aria-label="Slide 1"
-          ></button>
+        <div className="carousel-slider">
+          <img
+            className="h-65"
+            loading="lazy"
+            src="https://images.unsplash.com/photo-1532568547949-a09f1afe4d1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
         </div>
-
-        <div className="carousel-inner relative w-full overflow-hidden">
-          <div className="carousel-item active relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
-              className="block w-full"
-              alt="Motorbike Smoke"
-            />
-            <div className="carousel-caption hidden md:block absolute text-center">
-              <h5 className="text-xl">Welcome....</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
-          </div>
-
-          <div className="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
-              className="block w-full"
-              alt="Mountaintop"
-            />
-            <div className="carousel-caption hidden md:block absolute text-center">
-              <h5 className="text-xl">Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </div>
-
-          <div className="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
-              className="block w-full"
-              alt="Woman Reading a Book"
-            />
-            <div className="carousel-caption hidden md:block absolute text-center">
-              <h5 className="text-xl">Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </div>
+        <div>
+          <img
+            className="h-65"
+            loading="lazy"
+            src="https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
         </div>
-
-        <button
-          className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#carouselDarkVariant"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#carouselDarkVariant"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+        <div>
+          <img
+            className="h-65"
+            loading="lazy"
+            src="https://images.unsplash.com/photo-1585647347384-2593bc35786b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt=""
+          />
+        </div>
+      </Carousel>
     </div>
   );
 };
 
-export default Carousel;
+export default HCarousel;
